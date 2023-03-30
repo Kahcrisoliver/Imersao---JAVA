80757186163df75c1f5ed92437a97fc208b0df12
Karen Oliveira <kah.cris.oliver@gmail.com>
2023-03-30 07:54:23 -0300

    Atualização aula 02

--- src/App.java
+++ src/App.java
@@ -1,4 +1,7 @@
+import java.io.File;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -6,33 +9,65 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
+import javax.net.ssl.HttpsURLConnection;
+
+
 public class App {
+
     public static void main(String[] args) throws Exception {
-        // Fazer uma conexão HTTP e buscar os top 10 filmes
+
+
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI endereco = URI.create(url);
-        var cliente = HttpClient.newHttpClient(); 
-        HttpRequest requisicao = HttpRequest.newBuilder(endereco)
-            .GET()
-            .build();
-        HttpResponse<String> resposta = cliente.send(requisicao, BodyHandlers.ofString());  
-        String body = resposta.body();       
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
 
-        // Extrair só os dados que interessam (Ano, Título, Poster, Classificação)
-        JsonParser parser = new JsonParser();
+        
+        var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
 
-        // Exibir e manipular os dados
+
+        var diretorio = new File("figurinhas/");
+        diretorio.mkdir();
+
+        
         for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println("Ano: " + filme.get("year"));
-            System.out.println("Título: "+ filme.get("title"));
-            System.out.println("Poster: " + filme.get("image"));
+            var generator = new StickerGenerator();
+        
+            String UrlImagem = filme.get("image");
+            String titulo = filme.get("title");
+
             double classificacao = Double.parseDouble(filme.get("imDbRating"));
-            System.out.print("Classificação: ");
-            for (int i = 1; i <= classificacao; i++){
-                System.out.print("⭐");
+            int numeroEstrelas = (int) classificacao;
+
+            String textoSticker;
+            if (classificacao >= 8){
+                textoSticker = "TOP D+!";
+            } else if ( classificacao < 8 && classificacao > 4){
+                textoSticker = "NEM TANTO...";
+            } else {
+                textoSticker = "PERDA DE TEMPO...";
             }
-            System.out.print(" (" + filme.get("imDbRating") + ")");
+            
+            InputStream inputStream = new URL(UrlImagem).openStream();
+            String sticker = diretorio + titulo.replaceAll("[^a-zA-Z0-9.-]", " ") + ".png";
+
+
+            generator.cria(inputStream, sticker, textoSticker);
+
+            System.out.println("\u001b[44m\u001b[1mTítulo\u001b[m" + ": " + filme.get("title"));
+            System.out.println("\u001b[42m\u001b[1mURL da Imagem\u001b[m"+ ": "+filme.get("image"));
+            System.out.println("\u001b[43m\u001b[1mNota de Avaliação\u001b[m"+": "+filme.get("imDbRating"));
+            
+
+
+            for (int i = 1; i <= numeroEstrelas; i++){
+                System.out.print("⭐");    
+            
+            }
+
             System.out.println("\n");
         }
 
